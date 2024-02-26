acbbea40a3cbdc7386cb17cb27da42723b9f47f0
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-02-25 22:48:26 -0300

    feito tax in home

--- assets/js/functionsHome.js
+++ assets/js/functionsHome.js
@@ -1,14 +1,37 @@
-
 var produtos = JSON.parse(localStorage.getItem('db_produto')) ?? [];
 var categorias = JSON.parse(localStorage.getItem('db_category')) ?? [];
 var select = document.getElementById('select');
+var taxValue = document.getElementById("tax-value");
+
 produtos.forEach(function (produto) {
-    var option = document.createElement('option');
-    option.textContent = produto.product;
-    option.value = produto.id;
-    select.appendChild(option);
- 
+   var option = document.createElement('option');
+   option.textContent = produto.product;
+   option.value = produto.product;
+   select.appendChild(option);
 });
+
+select.addEventListener('change', function() {
+    var selectedProductName = this.value; 
+    var selectedProduct = produtos.find(function(produto) {
+        return produto.product === selectedProductName;
+    });
+
+    if (selectedProduct) {
+        var foundCategory = categorias.find(function(categoria) {
+            return categoria.category === selectedProduct.category;
+        });
+
+        if (foundCategory) {
+            taxValue.value = foundCategory.tax;
+        } else {
+            taxValue.value = "Categoria não encontrada";
+        }
+    } else {
+        taxValue.value = "Produto não encontrado";
+    }
+});
+
+
 
 /////////// area de teste
 
